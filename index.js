68b929a56f4f4664015230fdef85b6ff7a5df75c
NorWega <vaskevicpavel01@gmai.com>
2022-09-09 17:55:34 +0300

    dip_piano

---
+++ index.js
@@ -0,0 +1,45 @@
+// const pianoKeys = document.querySelectorAll('.key')
+
+// function playSound(newUrl) {
+//     console.log(newUrl)
+//     new Audio(newUrl).play()
+// }
+
+// pianoKeys.forEach((pianoKey, i) => {
+//     const number = i < 9 ? '0' + (i + 1) : (i + 1)
+//     const newUrl = '/piano-pad/key' + number + '.mp3'
+//     pianoKey.addEventListener('click', () => playSound(newUrl))
+// })
+
+const keys = document.querySelectorAll(".key"),
+  note = document.querySelector(".nowplaying"),
+  hints = document.querySelectorAll(".hints");
+
+function playNote(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`),
+    key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
+  if (!key) return;
+
+  const keyNote = key.getAttribute("data-note");
+
+  key.classList.add("playing");
+  note.innerHTML = keyNote;
+  audio.currentTime = 0;
+  audio.play();
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return;
+  this.classList.remove("playing");
+}
+
+function hintsOn(e, index) {
+  e.setAttribute("style", "transition-delay:" + index * 50 + "ms");
+}
+
+hints.forEach(hintsOn);
+
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+window.addEventListener("keydown", playNote);
